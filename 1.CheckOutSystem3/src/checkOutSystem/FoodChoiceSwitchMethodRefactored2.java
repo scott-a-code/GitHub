fb7096e3f3aab137865d21f8c41162eb95ed0430
scott-a-code <77281313+scott-a-code@users.noreply.github.com>
2021-02-18 16:19:39 +0000

    First Java Project

--- 1.CheckOutSystem3/src/checkOutSystem/FoodChoiceSwitchMethodRefactored2.java
+++ 1.CheckOutSystem3/src/checkOutSystem/FoodChoiceSwitchMethodRefactored2.java
@@ -2,6 +2,7 @@ package checkOutSystem;
 
 import static java.lang.System.out;
 
+import java.util.InputMismatchException;
 import java.util.TreeMap;
 
 import checkOutSystem.ifMethods.FoodIfMethods;
@@ -9,14 +10,12 @@ import checkOutSystem.items.Food;
 import checkOutSystem.items.Item;
 import checkOutSystem.listCreators.FoodListCreator;
 import checkOutSystem.mapScannerComparatorPaymentmethods.IncrementorDecrementorMethods;
-import checkOutSystem.mapScannerComparatorPaymentmethods.MapShoppingList;
 import checkOutSystem.mapScannerComparatorPaymentmethods.ScannerClass;
 
-public class FoodChoiceSwitchMethodRefactored {
+public class FoodChoiceSwitchMethodRefactored2 {
 
-	public static void foodChoiceSwitchMethod() throws InterruptedException {
-		MapShoppingList shopList = new MapShoppingList();
-		TreeMap<Item, Integer> shoppingList = shopList.getShoppingList();
+	public static void foodChoiceSwitchMethod(TreeMap<Item, Integer> shoppingList) throws InterruptedException {
+		try {
 		int specificItemChoice = ScannerClass.scanner.nextInt();
 		switch (specificItemChoice) {
 		case 101:
@@ -24,50 +23,50 @@ public class FoodChoiceSwitchMethodRefactored {
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Bread added to Basket\nEnter 1 to add another Bread\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethodsRefactored.whileIfLoop(specificItemChoice);
+			FoodIfMethods.breadWhileIfLoop(shoppingList);
 			break;
 		case 102:
 			foodChoice = FoodListCreator.getMilk();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Milk added to Basket\nEnter 1 to add another Milk\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.milkWhileIfLoop();
+			FoodIfMethods.milkWhileIfLoop(shoppingList);
 		case 103:
 			foodChoice = FoodListCreator.getCereal();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Cereal added to Basket\nEnter 1 to add another box of Cereal\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.cerealWhileIfLoop();
+			FoodIfMethods.cerealWhileIfLoop(shoppingList);
 		case 104:
 			foodChoice = FoodListCreator.getEggs();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Eggs added to Basket\nEnter 1 to add another box of Eggs\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.eggsWhileIfLoop();
+			FoodIfMethods.eggsWhileIfLoop(shoppingList);
 		case 105:
 			foodChoice = FoodListCreator.getBread();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Mince added to Basket\nEnter 1 to add another packet of Mince\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.minceWhileIfLoop();
+			FoodIfMethods.minceWhileIfLoop(shoppingList);
 		case 106:
 			foodChoice = FoodListCreator.getPasta();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Pasta added to Basket\nEnter 1 to add another packet of Pasta\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.pastaWhileIfLoop();
+			FoodIfMethods.pastaWhileIfLoop(shoppingList);
 		case 107:
 			foodChoice = FoodListCreator.getSoup();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Soup added to Basket\nEnter 1 to add another Soup\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.soupWhileIfLoop();
+			FoodIfMethods.soupWhileIfLoop(shoppingList);
 		case 108:
 			foodChoice = FoodListCreator.getApple();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Apples added to Basket\nEnter 1 to add another Apples\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.applesWhileIfLoop();
+			FoodIfMethods.applesWhileIfLoop(shoppingList);
 		default:
 			out.println(
 					"You have not selected an valid option, please select an option from the list or enter '0' to exit.");
@@ -78,66 +77,68 @@ public class FoodChoiceSwitchMethodRefactored {
 				System.exit(1);
 				break;
 			default:
-				FoodChoiceSwitchMethodRefactored.foodChoiceSwitchMethod(choice1);
+				FoodChoiceSwitchMethodRefactored2.foodChoiceSwitchMethod(shoppingList, choice1);
 			}
 			break;
 		}
+	} catch (InputMismatchException e) {
+
+	}
 	}
 
 	// overloaded method
-	public static void foodChoiceSwitchMethod(int choice1) throws InterruptedException {
-		MapShoppingList shopList = new MapShoppingList();
-		TreeMap<Item, Integer> shoppingList = shopList.getShoppingList();
+	public static void foodChoiceSwitchMethod(TreeMap<Item, Integer> shoppingList, int choice1) throws InterruptedException {
+		try {
 		switch (choice1) {
 		case 101:
 			Food foodChoice = FoodListCreator.getBread();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Bread added to Basket\nEnter 1 to add another Bread\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.breadWhileIfLoop();
+			FoodIfMethods.breadWhileIfLoop(shoppingList);
 			break;
 		case 102:
 			foodChoice = FoodListCreator.getMilk();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Milk added to Basket\nEnter 1 to add another Milk\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.milkWhileIfLoop();
+			FoodIfMethods.milkWhileIfLoop(shoppingList);
 		case 103:
 			foodChoice = FoodListCreator.getCereal();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Cereal added to Basket\nEnter 1 to add another box of Cereal\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.cerealWhileIfLoop();
+			FoodIfMethods.cerealWhileIfLoop(shoppingList);
 		case 104:
 			foodChoice = FoodListCreator.getEggs();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Eggs added to Basket\nEnter 1 to add another box of Eggs\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.eggsWhileIfLoop();
+			FoodIfMethods.eggsWhileIfLoop(shoppingList);
 		case 105:
 			foodChoice = FoodListCreator.getBread();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Mince added to Basket\nEnter 1 to add another packet of Mince\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.minceWhileIfLoop();
+			FoodIfMethods.minceWhileIfLoop(shoppingList);
 		case 106:
 			foodChoice = FoodListCreator.getPasta();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Pasta added to Basket\nEnter 1 to add another packet of Pasta\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.pastaWhileIfLoop();
+			FoodIfMethods.pastaWhileIfLoop(shoppingList);
 		case 107:
 			foodChoice = FoodListCreator.getSoup();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Soup added to Basket\nEnter 1 to add another Soup\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.soupWhileIfLoop();
+			FoodIfMethods.soupWhileIfLoop(shoppingList);
 		case 108:
 			foodChoice = FoodListCreator.getApple();
 			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
 			out.println(
 					"Apples added to Basket\nEnter 1 to add another Apples\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
-			FoodIfMethods.applesWhileIfLoop();
+			FoodIfMethods.applesWhileIfLoop(shoppingList);
 		default:
 			out.println(
 					"You have not selected an valid option, please select an option from the list or enter '0' to exit.");
@@ -148,9 +149,12 @@ public class FoodChoiceSwitchMethodRefactored {
 				System.exit(1);
 				break;
 			default:
-				FoodChoiceSwitchMethodRefactored.foodChoiceSwitchMethod(choice1);
+				FoodChoiceSwitchMethodRefactored2.foodChoiceSwitchMethod(shoppingList, choice1);
 			}
 			break;
 		}
+	} catch (InputMismatchException e) {
+
 	}
+}
 }
---
+++ 1.CheckOutSystem3/src/checkOutSystem/FoodIfMethodsRefactored2.java
@@ -0,0 +1,257 @@
+package checkOutSystem;
+
+import static java.lang.System.out;
+
+import java.util.InputMismatchException;
+import java.util.TreeMap;
+
+import checkOutSystem.choiceSwitchMethods.FoodChoiceSwitchMethod;
+import checkOutSystem.items.Food;
+import checkOutSystem.items.Item;
+import checkOutSystem.listCreators.FoodListCreator;
+import checkOutSystem.mapScannerComparatorPaymentmethods.IncrementorDecrementorMethods;
+import checkOutSystem.mapScannerComparatorPaymentmethods.PriceCalcAndPayment;
+import checkOutSystem.mapScannerComparatorPaymentmethods.ScannerClass;
+
+public class FoodIfMethodsRefactored2 {
+
+	public static void breadWhileIfLoop(TreeMap<Item, Integer> shoppingList) throws InterruptedException {
+		try {
+			int addRemoveOrReturn = ScannerClass.scanner.nextInt();
+			Food foodChoice = FoodListCreator.getBread();
+			while (addRemoveOrReturn == 1) {
+				IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoice);
+			out.println(
+					"Bread added to Basket\nEnter 1 to add another Bread\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
+				addRemoveOrReturn = ScannerClass.scanner.nextInt();
+		}
+			if (addRemoveOrReturn == 2) {
+			out.println(FoodListCreator.create());
+			FoodChoiceSwitchMethod.foodChoiceSwitchMethod(shoppingList);
+		} else if (addRemoveOrReturn == 3) {
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else if (addRemoveOrReturn == 4) {
+			PriceCalcAndPayment.finalPaymentMethod();
+		} else if (addRemoveOrReturn == 5) {
+			IncrementorDecrementorMethods.foodDerementationOfItemToList(shoppingList, foodChoice);
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else {
+			System.out.println("Invalid input, please choose a valid option");
+			FoodIfMethodsRefactored2.breadWhileIfLoop(shoppingList);
+		}
+	} catch (InputMismatchException e) {
+	}
+	}
+
+	public static void milkWhileIfLoop(TreeMap<Item, Integer> shoppingList) throws InterruptedException {
+		try {
+		int addRemoveOrReturnMilk = ScannerClass.scanner.nextInt();
+		Food foodChoiceMilk = FoodListCreator.getMilk();
+		while (addRemoveOrReturnMilk == 1) {
+			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoiceMilk);
+			out.println(
+					"Milk added to Basket\nEnter 1 to add another Milk\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
+			addRemoveOrReturnMilk = ScannerClass.scanner.nextInt();
+		}
+		if (addRemoveOrReturnMilk == 2) {
+			out.println(FoodListCreator.create());
+			FoodChoiceSwitchMethod.foodChoiceSwitchMethod(shoppingList);
+		} else if (addRemoveOrReturnMilk == 3) {
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else if (addRemoveOrReturnMilk == 4) {
+			PriceCalcAndPayment.finalPaymentMethod();
+		} else if (addRemoveOrReturnMilk == 5) {
+			IncrementorDecrementorMethods.foodDerementationOfItemToList(shoppingList, foodChoiceMilk);
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else {
+			System.out.println("Invalid input, please choose a valid option");
+			FoodIfMethodsRefactored2.milkWhileIfLoop(shoppingList);
+		}
+	} catch (InputMismatchException e) {
+	}
+	}
+
+	public static void cerealWhileIfLoop(TreeMap<Item, Integer> shoppingList) throws InterruptedException {
+		try {
+		int addRemoveOrReturnCereal = ScannerClass.scanner.nextInt();
+		Food foodChoiceCereal = FoodListCreator.getCereal();
+		while (addRemoveOrReturnCereal == 1) {
+			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoiceCereal);
+			out.println(
+					"Cereal added to Basket\nEnter 1 to add another box of Cereal\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
+			addRemoveOrReturnCereal = ScannerClass.scanner.nextInt();
+		}
+		if (addRemoveOrReturnCereal == 2) {
+			out.println(FoodListCreator.create());
+			FoodChoiceSwitchMethod.foodChoiceSwitchMethod(shoppingList);
+		} else if (addRemoveOrReturnCereal == 3) {
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else if (addRemoveOrReturnCereal == 4) {
+			PriceCalcAndPayment.finalPaymentMethod();
+		} else if (addRemoveOrReturnCereal == 5) {
+			IncrementorDecrementorMethods.foodDerementationOfItemToList(shoppingList, foodChoiceCereal);
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else {
+			System.out.println("Invalid input, please choose a valid option");
+			FoodIfMethodsRefactored2.cerealWhileIfLoop(shoppingList);
+		}
+	} catch (InputMismatchException e) {
+	}
+	}
+
+	public static void eggsWhileIfLoop(TreeMap<Item, Integer> shoppingList) throws InterruptedException {
+		try {
+		int addRemoveOrReturnEggs = ScannerClass.scanner.nextInt();
+		Food foodChoiceEggs = FoodListCreator.getEggs();
+		while (addRemoveOrReturnEggs == 1) {
+			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoiceEggs);
+			out.println(
+					"Eggs added to Basket\nEnter 1 to add another box of Eggs\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
+			addRemoveOrReturnEggs = ScannerClass.scanner.nextInt();
+		}
+		if (addRemoveOrReturnEggs == 2) {
+			out.println(FoodListCreator.create());
+			FoodChoiceSwitchMethod.foodChoiceSwitchMethod(shoppingList);
+		} else if (addRemoveOrReturnEggs == 3) {
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else if (addRemoveOrReturnEggs == 4) {
+			PriceCalcAndPayment.finalPaymentMethod();
+		} else if (addRemoveOrReturnEggs == 5) {
+			IncrementorDecrementorMethods.foodDerementationOfItemToList(shoppingList, foodChoiceEggs);
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else {
+			System.out.println("Invalid input, please choose a valid option");
+			FoodIfMethodsRefactored2.eggsWhileIfLoop(shoppingList);
+		}
+	} catch (InputMismatchException e) {
+	}
+	}
+
+	public static void minceWhileIfLoop(TreeMap<Item, Integer> shoppingList) throws InterruptedException {
+		try {
+		int addRemoveOrReturnMince = ScannerClass.scanner.nextInt();
+		Food foodChoiceMince = FoodListCreator.getMince();
+		while (addRemoveOrReturnMince == 1) {
+			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoiceMince);
+			out.println(
+					"Mince added to Basket\nEnter 1 to add another packet of Mince\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
+			addRemoveOrReturnMince = ScannerClass.scanner.nextInt();
+		}
+		if (addRemoveOrReturnMince == 2) {
+			out.println(FoodListCreator.create());
+			FoodChoiceSwitchMethod.foodChoiceSwitchMethod(shoppingList);
+		} else if (addRemoveOrReturnMince == 3) {
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else if (addRemoveOrReturnMince == 4) {
+			PriceCalcAndPayment.finalPaymentMethod();
+		} else if (addRemoveOrReturnMince == 5) {
+			IncrementorDecrementorMethods.foodDerementationOfItemToList(shoppingList, foodChoiceMince);
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else {
+			System.out.println("Invalid input, please choose a valid option");
+			FoodIfMethodsRefactored2.minceWhileIfLoop(shoppingList);
+		}
+	} catch (InputMismatchException e) {
+	}
+	}
+
+	public static void pastaWhileIfLoop(TreeMap<Item, Integer> shoppingList) throws InterruptedException {
+		try {
+		int addRemoveOrReturnPasta = ScannerClass.scanner.nextInt();
+		Food foodChoicePasta = FoodListCreator.getPasta();
+		while (addRemoveOrReturnPasta == 1) {
+			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoicePasta);
+			out.println(
+					"Pasta added to Basket\nEnter 1 to add another packet of Pasta\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
+			addRemoveOrReturnPasta = ScannerClass.scanner.nextInt();
+		}
+		if (addRemoveOrReturnPasta == 2) {
+			out.println(FoodListCreator.create());
+			FoodChoiceSwitchMethod.foodChoiceSwitchMethod(shoppingList);
+		} else if (addRemoveOrReturnPasta == 3) {
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else if (addRemoveOrReturnPasta == 4) {
+			PriceCalcAndPayment.finalPaymentMethod();
+		} else if (addRemoveOrReturnPasta == 5) {
+			IncrementorDecrementorMethods.foodDerementationOfItemToList(shoppingList, foodChoicePasta);
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else {
+			System.out.println("Invalid input, please choose a valid option");
+			FoodIfMethodsRefactored2.pastaWhileIfLoop(shoppingList);
+		}
+	} catch (InputMismatchException e) {
+	}
+	}
+
+	public static void soupWhileIfLoop(TreeMap<Item, Integer> shoppingList) throws InterruptedException {
+		try {
+		int addRemoveOrReturnSoup = ScannerClass.scanner.nextInt();
+		Food foodChoiceSoup = FoodListCreator.getSoup();
+		while (addRemoveOrReturnSoup == 1) {
+			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoiceSoup);
+			out.println(
+					"Soup added to Basket\nEnter 1 to add another tin of Soup\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
+			addRemoveOrReturnSoup = ScannerClass.scanner.nextInt();
+		}
+		if (addRemoveOrReturnSoup == 2) {
+			out.println(FoodListCreator.create());
+			FoodChoiceSwitchMethod.foodChoiceSwitchMethod(shoppingList);
+		} else if (addRemoveOrReturnSoup == 3) {
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else if (addRemoveOrReturnSoup == 4) {
+			PriceCalcAndPayment.finalPaymentMethod();
+		} else if (addRemoveOrReturnSoup == 5) {
+			IncrementorDecrementorMethods.foodDerementationOfItemToList(shoppingList, foodChoiceSoup);
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else {
+			System.out.println("Invalid input, please choose a valid option");
+			FoodIfMethodsRefactored2.soupWhileIfLoop(shoppingList);
+		}
+	} catch (InputMismatchException e) {
+	}
+	}
+
+	public static void applesWhileIfLoop(TreeMap<Item, Integer> shoppingList) throws InterruptedException {
+		try {
+		int addRemoveOrReturnApple = ScannerClass.scanner.nextInt();
+		Food foodChoiceApple = FoodListCreator.getApple();
+		while (addRemoveOrReturnApple == 1) {
+			IncrementorDecrementorMethods.foodIncrementationOfItemToList(shoppingList, foodChoiceApple);
+			out.println(
+					"Apples added to Basket\nEnter 1 to add another packet of Apples\nEnter 2 to return to Food list\nEnter 3 to return to the General item list\nEnter 4 to procced to checkout\nEnter 5 to remove item");
+			addRemoveOrReturnApple = ScannerClass.scanner.nextInt();
+		}
+		if (addRemoveOrReturnApple == 2) {
+			out.println(FoodListCreator.create());
+			FoodChoiceSwitchMethod.foodChoiceSwitchMethod(shoppingList);
+		} else if (addRemoveOrReturnApple == 3) {
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else if (addRemoveOrReturnApple == 4) {
+			PriceCalcAndPayment.finalPaymentMethod();
+		} else if (addRemoveOrReturnApple == 5) {
+			IncrementorDecrementorMethods.foodDerementationOfItemToList(shoppingList, foodChoiceApple);
+			out.println(FoodListCreator.create());
+			MainMenuOptionsMethod.mainMenuOptions();
+		} else {
+			System.out.println("Invalid input, please choose a valid option");
+			FoodIfMethodsRefactored2.applesWhileIfLoop(shoppingList);
+		}
+	} catch (InputMismatchException e) {
+	}
+	}
+}
